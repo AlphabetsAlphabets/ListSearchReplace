cc6591a9b323c6f7578c600530d0d9edb9bf7b2f
Yap Jia Hong <yapjiahong2003@gmail.com>
2020-10-31 15:33:25 +0800

    Successfully added new search feature.

---
+++ feature.py
@@ -0,0 +1,37 @@
+def search(board, element_to_search, nested=False):
+    ltr = 0
+    rtl = -1
+
+    guard = 0
+    limit = len(board)
+
+    ets = element_to_search
+    if nested == False:
+        while guard < limit:
+            try:
+                if board[ltr] == ets:
+                    return ltr
+                elif board[rtl] == find:
+                    return rtl
+                else:
+                    ltr += 1
+                    rtl -= 1
+            except IndexError:
+                guard += 1
+                continue
+
+    else:
+        while guard < limit:
+            for index, list1 in enumerate(board):
+                    try:
+                        if list1[ltr] == ets:
+                            return index, ltr
+                        elif list1[rtl] == ets:
+                            return index, rtl
+                        else:
+                            ltr += 1
+                            rtl -= 1
+
+                    except IndexError:
+                        guard += 1
+                        continue
